9ab3f153a51cbaba97ca864d8454c7208f993175
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-04-11 15:18:07 -0400

    feat(project): add pre-commit, github actions file, flake and editorconfig files

--- apps/menus/views.py
+++ apps/menus/views.py
@@ -1,6 +1,6 @@
 """Views for Menus App."""
 
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
